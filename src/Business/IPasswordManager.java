ed1cfc999a9ce1e1d4089e144e4724856129bbc3
Serhat Kumas <serhat.kumas@hotmail.com>
2022-12-29 01:16:22 +0300

    Password manager and exception throwing is implemented

--- src/Business/IPasswordManager.java
+++ src/Business/IPasswordManager.java
@@ -10,15 +10,15 @@ public interface IPasswordManager {
 
     List<PasswordRecord> getAllSocialMediaRecords(IPasswordDal passwordDal, Statement statement);
     PasswordRecord getSocialMediaRecordByRecordId(IPasswordDal passwordDal, Statement statement, String recordId);
-    List<PasswordRecord> getSocialMediaRecordsByUserId(IPasswordDal passwordDal, Statement statement, String userId);
+    List<PasswordRecord> getSocialMediaRecordsByUserId(IPasswordDal passwordDal, Statement statement, String userId) throws Exception;
     String getSocialMediaNameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId);
     String getSocialMediaLinkByRecordId(IPasswordDal passwordDal, Statement statement, String recordId);
     String getSocialMediaUsernameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId);
     String getSocialMediaPasswordContentByRecordId(IPasswordDal passwordDal, Statement statement, String recordId);
-    void updateSocialMediaNameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String SocialMediaSiteName);
-    void updateSocialMediaLinkByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String SocialMediaSiteLink);
-    void updateSocialMediaUsernameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String SocialMediaUsername);
-    void updateSocialMediaPasswordContentByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String SocialMediaPasswordContent);
+    void updateSocialMediaNameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String socialMediaSiteName);
+    void updateSocialMediaLinkByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String socialMediaSiteLink);
+    void updateSocialMediaUsernameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String socialMediaUsername);
+    void updateSocialMediaPasswordContentByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String socialMediaPasswordContent);
     void deleteSocialMediaRecordByRecordId(IPasswordDal passwordDal, Statement statement, String recordId);
-    void addSocialMediaRecordByRecordId(IPasswordDal passwordDal, Statement statement, Long recordId, Long userId, String socialMediaSiteName, String socialMediaSiteLink, String socialMediaUserName, String socialMediaPassword);
+    void addSocialMediaRecordByRecordId(IPasswordDal passwordDal, Statement statement, Long recordId, Long userId, String socialMediaSiteName, String socialMediaSiteLink, String socialMediaUserName, String socialMediaPassword) throws Exception;
 }
---
+++ src/Business/Implementations/PasswordManager.java
@@ -0,0 +1,93 @@
+package Business.Implementations;
+
+import Business.IPasswordManager;
+import Dal.IPasswordDal;
+import Model.PasswordRecord;
+
+import java.sql.Statement;
+import java.util.List;
+
+public class PasswordManager implements IPasswordManager {
+    @Override
+    public List<PasswordRecord> getAllSocialMediaRecords(IPasswordDal passwordDal, Statement statement) {
+        List<PasswordRecord> allSocialMediaRecords = passwordDal.getAllSocialMediaRecords(statement);
+        return allSocialMediaRecords;
+    }
+
+    @Override
+    public PasswordRecord getSocialMediaRecordByRecordId(IPasswordDal passwordDal, Statement statement, String recordId) {
+        PasswordRecord passwordRecord = passwordDal.getSocialMediaRecordByRecordId(statement, recordId);
+        return passwordRecord;
+    }
+
+    @Override
+    public List<PasswordRecord> getSocialMediaRecordsByUserId(IPasswordDal passwordDal, Statement statement, String userId) throws Exception {
+        List<PasswordRecord> allSocialMediaRecords = null ;
+        if(userId.length() == 11){
+            allSocialMediaRecords = passwordDal.getSocialMediaRecordsByUserId(statement, userId);
+            return allSocialMediaRecords;
+        }
+        else {
+            throw new Exception("User Id has to 11 character");
+        }
+    }
+
+    @Override
+    public String getSocialMediaNameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId) {
+        String socialMediaSiteName = passwordDal.getSocialMediaNameByRecordId(statement, recordId);
+        return socialMediaSiteName;
+    }
+
+    @Override
+    public String getSocialMediaLinkByRecordId(IPasswordDal passwordDal, Statement statement, String recordId) {
+        String socialMediaSiteLink = passwordDal.getSocialMediaLinkByRecordId(statement, recordId);
+        return socialMediaSiteLink;
+    }
+
+    @Override
+    public String getSocialMediaUsernameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId) {
+        String socialMediaUsername = passwordDal.getSocialMediaUsernameByRecordId(statement, recordId);
+        return socialMediaUsername;
+    }
+
+    @Override
+    public String getSocialMediaPasswordContentByRecordId(IPasswordDal passwordDal, Statement statement, String recordId) {
+        String socialMediaPasswordContent = passwordDal.getSocialMediaPasswordContentByRecordId(statement, recordId);
+        return socialMediaPasswordContent;
+    }
+
+    @Override
+    public void updateSocialMediaNameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String socialMediaSiteName) {
+        passwordDal.updateSocialMediaNameByRecordId(statement, recordId, socialMediaSiteName);
+    }
+
+    @Override
+    public void updateSocialMediaLinkByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String socialMediaSiteLink) {
+        passwordDal.updateSocialMediaLinkByRecordId(statement, recordId, socialMediaSiteLink);
+    }
+
+    @Override
+    public void updateSocialMediaUsernameByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String socialMediaUsername) {
+        passwordDal.updateSocialMediaUsernameByRecordId(statement, recordId, socialMediaUsername);
+    }
+
+    @Override
+    public void updateSocialMediaPasswordContentByRecordId(IPasswordDal passwordDal, Statement statement, String recordId, String socialMediaPasswordContent) {
+        passwordDal.updateSocialMediaPasswordContentByRecordId(statement, recordId, socialMediaPasswordContent);
+    }
+
+    @Override
+    public void deleteSocialMediaRecordByRecordId(IPasswordDal passwordDal, Statement statement, String recordId) {
+        passwordDal.deleteSocialMediaRecordByRecordId(statement, recordId);
+    }
+
+    @Override
+    public void addSocialMediaRecordByRecordId(IPasswordDal passwordDal, Statement statement, Long recordId, Long userId, String socialMediaSiteName, String socialMediaSiteLink, String socialMediaUserName, String socialMediaPassword) throws Exception {
+        if (userId.toString().length() == 11){
+            passwordDal.addSocialMediaRecordByRecordId(statement, recordId, userId, socialMediaSiteName, socialMediaSiteLink, socialMediaUserName, socialMediaPassword);
+        }
+        else {
+            throw new Exception("User Id has to 11 character");
+        }
+    }
+}
