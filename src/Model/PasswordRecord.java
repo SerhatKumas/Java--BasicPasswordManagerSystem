92aec26cd9c4b41820fa689189bf6d001a8798c4
Serhat Kumas <serhat.kumas@hotmail.com>
2022-12-28 18:51:15 +0300

    Syntax is fixed in Password record

--- src/Model/PasswordRecord.java
+++ src/Model/PasswordRecord.java
@@ -6,15 +6,15 @@ public class PasswordRecord {
     private final Long userId;
     private String socialMediaSiteName;
     private String socialMediaSiteLink;
-    private String socialMediaUserName;
+    private String socialMediaUsername;
     private String socialMediaPassword;
 
-    public PasswordRecord(Long recordId, Long userId, String socialMediaSiteName, String socialMediaSiteLink, String socialMediaUserName, String socialMediaPassword) {
+    public PasswordRecord(Long recordId, Long userId, String socialMediaSiteName, String socialMediaSiteLink, String socialMediaUsername, String socialMediaPassword) {
         this.recordId = recordId;
         this.userId = userId;
         this.socialMediaSiteName = socialMediaSiteName;
         this.socialMediaSiteLink = socialMediaSiteLink;
-        this.socialMediaUserName = socialMediaUserName;
+        this.socialMediaUsername = socialMediaUsername;
         this.socialMediaPassword = socialMediaPassword;
     }
 }
