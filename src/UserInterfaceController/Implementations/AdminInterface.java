872d94e27edb367556bcaf0509284e6d6db0138a
Serhat Kumas <serhatkumas@outlook.com>
2023-01-01 23:32:06 +0300

    admin portal is implemented

--- src/UserInterfaceController/Implementations/AdminInterface.java
+++ src/UserInterfaceController/Implementations/AdminInterface.java
@@ -1,12 +1,14 @@
 package UserInterfaceController.Implementations;
 
-import Business.IPasswordManager;
-import Business.IUserManager;
-import UserInterfaceController.IInterfaceController;
+import Business.*;
+import Dal.Implementations.*;
+import Model.*;
+import UserInterfaceController.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 import java.util.Scanner;
 
 public class AdminInterface implements IInterfaceController {
@@ -22,12 +24,128 @@ public class AdminInterface implements IInterfaceController {
 
     @Override
     public void createPortal(Scanner scanner) {
-
+        List<PasswordRecord> passwordRecordList = null;
+        List<User> userList = null;
+        String userId = "";
+        String recordId = "";
+        User user = null;
+        while(true){
+            printPortalMethods();
+            int menuChoice = scanner.nextInt();
+            switch (menuChoice){
+                case 1:
+                    passwordRecordList = passwordManager.getAllSocialMediaRecords(new PasswordRecordDal(),statement);
+                    System.out.println(passwordRecordList.toString());
+                case 2:
+                    System.out.println("Enter user id : ");
+                    userId = scanner.next();
+                    try {
+                        passwordRecordList = passwordManager.getSocialMediaRecordsByUserId(new PasswordRecordDal(),statement,userId);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(passwordRecordList.toString());
+                    break;
+                case 3:
+                    System.out.println("Enter record id : ");
+                    recordId = scanner.next();
+                    PasswordRecord passwordRecord = passwordManager.getSocialMediaRecordByRecordId(new PasswordRecordDal(), statement, recordId);
+                    System.out.println(passwordRecord.toString());
+                    break;
+                case 4:
+                    try {
+                        System.out.println("Enter social media name : ");
+                        String socialMediaNameSearch = scanner.next();
+                        System.out.println("Enter social media name : ");
+                        String socialMediaName = scanner.next();
+                        passwordRecordList = passwordManager.getSocialMediaRecordsBySocialMediaSiteName(new PasswordRecordDal(),statement,socialMediaNameSearch,socialMediaName);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(passwordRecordList.toString());
+                    break;
+                case 5:
+                    System.out.println("Enter user id : ");
+                    userId = scanner.next();
+                    System.out.println("Enter social media name : ");
+                    String socialMediaName = scanner.next();
+                    System.out.println("Enter social media link : ");
+                    String socialMediaLink = scanner.next();
+                    System.out.println("Enter social media username : ");
+                    String socialMediaUsername = scanner.next();
+                    System.out.println("Enter social media password : ");
+                    String socialMediaPassword = scanner.next();
+                    try {
+                        passwordManager.addSocialMediaRecordByRecordId(new PasswordRecordDal(), statement, (long) getRecordId(), Long.valueOf(userId),socialMediaName,socialMediaLink,socialMediaUsername,socialMediaPassword);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case 6:
+                    System.out.println("Enter record id : ");
+                    recordId = scanner.next();
+                    passwordManager.deleteSocialMediaRecordByRecordId(new PasswordRecordDal(), statement, recordId);
+                    break;
+                case 7:
+                    userList = userManager.getAllUsers(new UserDal(), statement);
+                    System.out.println(userList.toString());
+                    break;
+                case 8:
+                    System.out.println("Enter user id : ");
+                    userId = scanner.next();
+                    try {
+                        user = userManager.getUserByUserId(new UserDal(), statement,userId);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(user.toString());
+                    break;
+                case 9:
+                    System.out.println("Enter user name : ");
+                    String userName = scanner.next();
+                    System.out.println("Enter user surname : ");
+                    String userSurname = scanner.next();
+                    System.out.println("Enter user email : ");
+                    String userEmail = scanner.next();
+                    System.out.println("Enter user password : ");
+                    String userPassword = scanner.next();
+                    try {
+                        userManager.addUser(new UserDal(), statement, (long )getUserId(),userName,userSurname, userEmail, userPassword);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case 10:
+                    System.out.println("Enter user id : ");
+                    userId = scanner.next();
+                    try {
+                        userManager.deleteUserByUserId(new UserDal(), statement, userId);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                case 11:
+                    System.out.println("System is closing...");
+                    System.exit(0);
+                default:
+                    System.err.println("Enter valid menu option");
+            }
+        }
     }
 
     @Override
     public void printPortalMethods() {
-        String portalMethods = "1)Display All Passwords In The System\n2)Display User's All Passwords By User Id\n3)Display User's Password By Password Id\n4)Display All Passwords By Social Media Site Name\n5)Display Specific User's Passwords By User Id and Social Media Site Name\n6)Create New Password\n7)Create New User\n8)Delete Password By Id\n9)Delete User and It's All Passwords";
+        String portalMethods = """
+                1)Display All Passwords In The System
+                2)Display User's All Passwords By User Id
+                3)Display User's Password By Password Id
+                4)Display Specific User's Passwords By User Id and Social Media Site Name
+                5)Create New Password
+                6)Delete Password By Id
+                7)Display All User In The System
+                8)Display User By Id
+                9)Create New User
+                10)Delete User By Id
+                11)Exit the System""";
         System.out.println(portalMethods);
     }
 
@@ -37,6 +155,19 @@ public class AdminInterface implements IInterfaceController {
         int recordId ;
         try {
             rs = statement.executeQuery("select count(*) from passwordrecordtable");
+            rs.next();
+            recordId = rs.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return recordId+1;
+    }
+
+    private int getUserId() {
+        ResultSet rs ;
+        int recordId ;
+        try {
+            rs = statement.executeQuery("select count(*) from usertable");
             rs.next();
             recordId = rs.getInt(1);
         } catch (SQLException e) {
